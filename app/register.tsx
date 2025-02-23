d87a1dddd6e6dfa5ffa3530228ac8c1f16e1771e
IanBruch <ibruch@purdue.edu>
2025-02-23 15:35:12 -0500

    Register GUI implemented

--- app/register.tsx
+++ app/register.tsx
@@ -10,25 +10,7 @@ import { Link, LinkText } from "@/components/ui/link";
 import { useQuery } from "@tanstack/react-query";
 import { getLoginState, loginUser } from "@/services/asyncStorageServices";
 
-
-function LoginAttempt(email:string|null|undefined, password:string|null|undefined) {
-  if (!(typeof email === 'string' && typeof password === 'string')) { //TODO ask if there is a better method here
-    console.log("returning false")
-    return "false"
-  }
-  const { data: login_attempt } = useQuery({
-    queryKey: [email, password],
-    queryFn: async ({queryKey}) => {
-      const [email, password] = queryKey
-      console.log(email + " " + password)
-      const state = await loginUser(email, password);
-      return state;
-    },
-  })
-  return login_attempt
-}
-
-export default function LoginScreen() {
+export default function RegisterScreen() {
   const router = useRouter();
   const { data: login_state } = useQuery({
     queryKey: ["logged-in"],
@@ -47,8 +29,8 @@ export default function LoginScreen() {
 
       <Card variant="ghost" className="p-10 mb-50">
           <VStack space="sm" className="mb-50">
-            <Heading size="4xl">Sign In</Heading>
-            <Text size="lg">Sign Into Your Kova Account</Text>
+            <Heading size="4xl">Account Registration</Heading>
+            <Text size="lg">Register for Kova</Text>
           </VStack>
       </Card>
       <Card variant="outline" className="m-[25px]">
@@ -63,25 +45,17 @@ export default function LoginScreen() {
           <Input className="ml-3 mr-5">
             <InputField id="password_input" placeholder="Enter Password" />
         </Input>
+        <Text size="lg" className="ml-3 mt-5">Confirm Password</Text>
+          <Input className="ml-3 mr-5">
+            <InputField id="password_input" placeholder="Enter Password" />
+        </Input>
         <Button variant="solid" size="xl" action="secondary" className="mt-5 mb-5 bg-[#6FA8DC]"
           onPress={() => router.replace("/(tabs)/workout")}
           > {/* TODO implement button routing and login features */}
-          <ButtonText className="text-white">Sign In</ButtonText>
+          <ButtonText className="text-white">Register For Account</ButtonText>
         </Button>
-          <Link>  {/* TODO actually add this link to a new page */} 
-            <LinkText>Forgot Password?</LinkText>
-            {/* TODO ask abt LinkText error */}
-          </Link>
         </VStack>
       </Card>
-      <VStack space="sm">
-        <Heading className="text-center">New To Kova?</Heading>
-        <Button size="xl" className="ml-[38px] mr-[38px] bg-[#6FA8DC]"
-        onPress={() => router.replace("../register")}
-        >
-          <ButtonText className="text-white">Register for Account</ButtonText>
-        </Button>
-      </VStack>
     </Container>
   );
 }
