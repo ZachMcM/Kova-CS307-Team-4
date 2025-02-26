3e4bc36af692cbe20f72303b22abf1c55c87d068
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-02-25 19:11:44 -0500

    Fixing text strings empty error

--- app/login.tsx
+++ app/login.tsx
@@ -1,22 +1,21 @@
-import { Card } from "@/components/ui/card";
-import { VStack } from "@/components/ui/vstack";
-import { Text } from "@/components/ui/text";
-import { Heading } from "@/components/ui/heading";
 import Container from "@/components/Container";
-import { Input, InputField } from "@/components/ui/input";
 import { Button, ButtonText } from "@/components/ui/button";
-import { Redirect, useRouter } from "expo-router";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
 import { Link, LinkText } from "@/components/ui/link";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { getLoginState, loginUser } from "@/services/asyncStorageServices";
-import { useState } from "react";
+import { Text } from "@/components/ui/text";
 import { useToast } from "@/components/ui/toast";
+import { VStack } from "@/components/ui/vstack";
+import { getLoginState, loginUser } from "@/services/asyncStorageServices";
 import { showErrorToast } from "@/services/toastServices";
-
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Redirect, useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const toast = useToast();
 
@@ -27,54 +26,70 @@ export default function LoginScreen() {
       const state = await getLoginState();
       return state;
     },
-  })
+  });
 
-  const {mutate: login} = useMutation({
+  const { mutate: login } = useMutation({
     mutationFn: async () => {
-      console.log(email + " " + password)
+      console.log(email + " " + password);
       const state = await loginUser(email, password);
       return state;
     },
     onSuccess: () => {
-      router.replace("/(tabs)")
+      router.replace("/(tabs)");
     },
     onError: (e) => {
       console.log(e);
       showErrorToast(toast, e.message);
-    }
-  })
+    },
+  });
 
   return (
-    
     <Container>
-      {(login_state==="true") ? (
-              <Redirect href={"/"}></Redirect>
-            ) : (<> </>)} 
-
+      {login_state === "true" ? <Redirect href={"/"}></Redirect> : <></>}
       <Card variant="ghost" className="p-10 mb-50">
-          <VStack space="sm" className="mb-50">
-            <Heading size="4xl">Sign In</Heading>
-            <Text size="lg">Sign Into Your Kova Account</Text>
-          </VStack>
+        <VStack space="sm" className="mb-50">
+          <Heading size="4xl">Sign In</Heading>
+          <Text size="lg">Sign Into Your Kova Account</Text>
+        </VStack>
       </Card>
       <Card variant="outline" className="m-[25px]">
         <VStack space="sm">
-          <Text size="lg" className="ml-3 mt-5">Email</Text>
+          <Text size="lg" className="ml-3 mt-5">
+            Email
+          </Text>
           <Input className="ml-3 mr-5">
-            <InputField value={email} onChangeText={setEmail} placeholder="Enter Email" />
-        </Input>
+            <InputField
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Enter Email"
+            />
+          </Input>
         </VStack>
         <VStack space="sm">
-          <Text size="lg" className="ml-3 mt-5">Password</Text>
+          <Text size="lg" className="ml-3 mt-5">
+            Password
+          </Text>
           <Input className="ml-3 mr-5">
-            <InputField value={password} onChangeText={setPassword} placeholder="Enter Password" />
-        </Input>
-        <Button variant="solid" size="xl" action="secondary" className="mt-5 mb-5 bg-[#6FA8DC]"
-          onPress={() => {login()}}
-          > {/* TODO implement button routing and login features */}
-          <ButtonText className="text-white">Sign In</ButtonText>
-        </Button>
-          <Link>  {/* TODO actually add this link to a new page */} 
+            <InputField
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Enter Password"
+            />
+          </Input>
+          <Button
+            variant="solid"
+            size="xl"
+            action="secondary"
+            className="mt-5 mb-5 bg-[#6FA8DC]"
+            onPress={() => {
+              login();
+            }}
+          >
+            {/* TODO implement button routing and login features */}
+            <ButtonText className="text-white">Sign In</ButtonText>
+          </Button>
+          <Link>
+            {/* TODO actually add this link to a new page */}
             <LinkText>Forgot Password?</LinkText>
             {/* TODO ask abt LinkText error */}
           </Link>
@@ -82,8 +97,10 @@ export default function LoginScreen() {
       </Card>
       <VStack space="sm">
         <Heading className="text-center">New To Kova?</Heading>
-        <Button size="xl" className="ml-[38px] mr-[38px] bg-[#6FA8DC]"
-        onPress={() => router.replace("./register")}
+        <Button
+          size="xl"
+          className="ml-[38px] mr-[38px] bg-[#6FA8DC]"
+          onPress={() => router.replace("./register")}
         >
           <ButtonText className="text-white">Register for Account</ButtonText>
         </Button>
--- app/register.tsx
+++ app/register.tsx
@@ -1,22 +1,24 @@
-import { Card } from "@/components/ui/card";
-import { VStack } from "@/components/ui/vstack";
-import { Text } from "@/components/ui/text";
-import { Heading } from "@/components/ui/heading";
 import Container from "@/components/Container";
-import { Input, InputField } from "@/components/ui/input";
 import { Button, ButtonText } from "@/components/ui/button";
-import { Redirect, useRouter } from "expo-router";
-import { Link, LinkText } from "@/components/ui/link";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { getLoginState, loginUser, registerAccount } from "@/services/asyncStorageServices";
-import { useState } from "react";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
 import { useToast } from "@/components/ui/toast";
+import { VStack } from "@/components/ui/vstack";
+import {
+  getLoginState,
+  registerAccount,
+} from "@/services/asyncStorageServices";
 import { showErrorToast } from "@/services/toastServices";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Redirect, useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function RegisterScreen() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const toast = useToast();
 
@@ -27,59 +29,78 @@ export default function RegisterScreen() {
       const state = await getLoginState();
       return state;
     },
-  })
+  });
 
-  const { mutate : register } = useMutation({
+  const { mutate: register } = useMutation({
     mutationFn: async () => {
       if (password === confirmPassword) {
         const state = await registerAccount(email, password);
       } else {
-        throw new Error("Password and Confirm Password do not match")
+        throw new Error("Password and Confirm Password do not match");
       }
     },
     onSuccess: () => {
-      router.replace("/(tabs)/profile")
+      router.replace("/(tabs)/profile");
     },
     onError: (e) => {
       console.log(e);
       showErrorToast(toast, e.message);
     },
-  })
+  });
 
   return (
-    
     <Container>
-      {(login_state==="true") ? (
-              <Redirect href={"/"}></Redirect>
-            ) : (<> </>)} 
-
+      {login_state === "true" ? <Redirect href={"/"}></Redirect> : <></>}
       <Card variant="ghost" className="p-10 mb-50">
-          <VStack space="sm" className="mb-50">
-            <Heading size="4xl">Account Registration</Heading>
-            <Text size="lg">Register for Kova</Text>
-          </VStack>
+        <VStack space="sm" className="mb-50">
+          <Heading size="4xl">Account Registration</Heading>
+          <Text size="lg">Register for Kova</Text>
+        </VStack>
       </Card>
       <Card variant="outline" className="m-[25px]">
         <VStack space="sm">
-          <Text size="lg" className="ml-3 mt-5">Email</Text>
+          <Text size="lg" className="ml-3 mt-5">
+            Email
+          </Text>
           <Input className="ml-3 mr-5">
-            <InputField value={email} onChangeText={setEmail} placeholder="Enter Email" />
-        </Input>
+            <InputField
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Enter Email"
+            />
+          </Input>
         </VStack>
         <VStack space="sm">
-          <Text size="lg" className="ml-3 mt-5">Password</Text>
+          <Text size="lg" className="ml-3 mt-5">
+            Password
+          </Text>
           <Input className="ml-3 mr-5">
-            <InputField value={password} onChangeText={setPassword} placeholder="Enter Password" />
-        </Input>
-        <Text size="lg" className="ml-3 mt-5">Confirm Password</Text>
+            <InputField
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Enter Password"
+            />
+          </Input>
+          <Text size="lg" className="ml-3 mt-5">
+            Confirm Password
+          </Text>
           <Input className="ml-3 mr-5">
-            <InputField value={confirmPassword} onChangeText={setConfirmPassword} placeholder="Enter Password" />
-        </Input>
-        <Button variant="solid" size="xl" action="secondary" className="mt-5 mb-5 bg-[#6FA8DC]"
-          onPress={() => register()}
-          > {/* TODO implement button routing and login features */}
-          <ButtonText className="text-white">Register For Account</ButtonText>
-        </Button>
+            <InputField
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              placeholder="Enter Password"
+            />
+          </Input>
+          <Button
+            variant="solid"
+            size="xl"
+            action="secondary"
+            className="mt-5 mb-5 bg-[#6FA8DC]"
+            onPress={() => register()}
+          >
+            {/* TODO implement button routing and login features */}
+            <ButtonText className="text-white">Register For Account</ButtonText>
+          </Button>
         </VStack>
       </Card>
     </Container>
