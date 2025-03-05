0d48a588f4ccad33f182153bc1f20bce8b3edaa2
Areeb Emran <areebemran@gmail.com>
2025-03-05 17:11:57 -0500

    Trying to merge

--- components/search-views/ExerciseSearchView.tsx
+++ components/search-views/ExerciseSearchView.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { Card } from "../ui/card";
 import { ExtendedExercise } from "@/types/extended-types";
+<<<<<<< Updated upstream
 import { View } from "react-native-reanimated/lib/typescript/Animated";
 import { TaggedSearchItem, exercisesToSearch} from "@/types/searcher-types";
 
@@ -9,5 +10,19 @@ export default function ExerciseSearchView({ data, selectionCallback, displayGet
     TaggedSearchItem[]
     return ;
 =======
+import { TaggedSearchItem, exercisesToSearch } from "@/types/searcher-types";
+import TaggedSearchView from "./skeletons/TaggedSearchView";
+
+export default function ExerciseSearchView({ data, selectionCallback} : 
+        { data: ExtendedExercise[], selectionCallback: (id: string) => void}){
+    let items = exercisesToSearch(data);
+    return TaggedSearchView(data, selectionCallback, (id) => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id = id) {
+                return new ExerciseCard
+            }
+        }
+        return null;
+    });
 >>>>>>> Stashed changes
 }
